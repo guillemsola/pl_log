b0978b5548f3849c70400deb69df447f128b16bf
guillemsola <guillemsola@gmail.com>
2014-10-13 23:45:16 +0200

    Clean up samples
    Moved and rename old ones
    Add new ones in a samples folder

--- routes/log.js
+++ routes/log.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET log . */
 router.get('/', function(req, res) {
-	var readLog = new Log('log.csv');
+	var readLog = new Log('./samples/bpm output SRF with GV ok.csv');
 
 	readLog.on('end', function(data) { 
 		res.send(data);
